10d8f931e51ec767407c1cb82ce83abb05d1c04e
lunana2525 <lunalamenti@gmail.com>
2025-04-22 21:37:55 +0700

    update.tsx

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,4 @@
-import { Lacquer} from "next/font/google";
-import { Chilanka} from "next/font/google";
-import { Cinzel} from "next/font/google";
+import { Lacquer, Chilanka, Cinzel} from "next/font/google";
 
 export const cinzel = Cinzel({ subsets: ["latin"] });
 export const lacquer = Lacquer({ weight: "400", subsets: ["latin"] });
