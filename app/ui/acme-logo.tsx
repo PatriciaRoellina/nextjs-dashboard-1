536466058ae7cd20e3841a35e1f99df11f281f38
lunana2525 <lunalamenti@gmail.com>
2025-04-22 20:28:58 +0700

    update code.tsx

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,10 +1,10 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { chilanka } from '@/app/ui/fonts'; // Ubah lusitana menjadi chilanka
+import { chilanka } from '@/app/ui/fonts'; // Ubah chilanka menjadi chilanka
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${chilanka.className} flex flex-row items-center leading-none text-white`} // Ganti lusitana dengan chilanka
+      className={`${chilanka.className} flex flex-row items-center leading-none text-white`} // Ganti chilanka dengan chilanka
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
